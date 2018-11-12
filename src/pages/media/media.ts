423e843816aa90330750f2f8414438ce409900fc
anish <anishvisaria98@gmail.com>
2018-11-11 22:56:46 -0500

    lastfm full list

--- src/pages/media/media.ts
+++ src/pages/media/media.ts
@@ -10,11 +10,16 @@ import { Http } from '@angular/http';
 })
 export class MediaPage {
 
+  items: any;
+
   constructor(public navCtrl: NavController, public navParams: NavParams, public httpm: Http) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MediaPage');
+  }
+
+  ionViewWillEnter() {
     this.fetchMedia()
   }
 
@@ -24,6 +29,7 @@ export class MediaPage {
       'http://ws.audioscrobbler.com/2.0/?method=album.search&album='+searchQuery+'&api_key=40d539e1ec8e98ccce917bdfe054e5a7&format=json'
     ).subscribe(data => {
       console.log(data.json())
+      this.items = data.json().results.albummatches.album
     });
   }
 
