b67ffd48bb26a1a638620f90a03efaf02f1fda30
anish <anishvisaria98@gmail.com>
2018-11-25 23:22:58 -0500

    videos quotes music

--- src/pages/media/media.ts
+++ src/pages/media/media.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { Http } from '@angular/http';
+import { Http, RequestOptions, Headers } from '@angular/http';
 
 
 @IonicPage()
@@ -11,8 +11,11 @@ import { Http } from '@angular/http';
 export class MediaPage {
 
   items: any;
+  quotesList: any;
+  selectedSegment: string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public httpm: Http) {
+    this.selectedSegment = 'music';
   }
 
   ionViewDidLoad() {
@@ -21,15 +24,35 @@ export class MediaPage {
 
   ionViewWillEnter() {
     this.fetchMedia()
+    this.fetchQuotes()
   }
 
   fetchMedia() {
     let searchQuery = 'happy';
     this.httpm.get(
-      'http://ws.audioscrobbler.com/2.0/?method=album.search&album='+searchQuery+'&api_key=40d539e1ec8e98ccce917bdfe054e5a7&format=json'
+      'http://ws.audioscrobbler.com/2.0/?method=album.search&album=' + searchQuery + '&api_key=40d539e1ec8e98ccce917bdfe054e5a7&format=json'
     ).subscribe(data => {
       console.log(data.json())
       this.items = data.json().results.albummatches.album
+      console.log(this.items)
+    });
+  }
+
+  fetchQuotes() {
+    let opt: RequestOptions
+    let myHeaders: Headers = new Headers
+    myHeaders.set('X-Mashape-Key', '0BbcWISiTJmshpeV8A9yMSbBxeOMp13mRjdjsnY5BGfKdh4UNM');
+    myHeaders.append('Accept', 'application/json');
+    opt = new RequestOptions({
+      headers: myHeaders
+    })
+
+    this.httpm.get(
+      'https://andruxnet-random-famous-quotes.p.mashape.com/?count=20', opt
+    ).subscribe(data => {
+      console.log(data.json())
+      this.quotesList = data.json();
+      // this.q = data.json().results.albummatches.album
     });
   }
 
